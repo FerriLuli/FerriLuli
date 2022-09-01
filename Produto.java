4b180fcb1b8064056958e5e788d1a82d190f221f
FerriLuli <110925223+FerriLuli@users.noreply.github.com>
2022-09-01 14:39:40 -0300

    Delete Produto.java

--- Produto.java
+++
@@ -1,8 +0,0 @@
-package Livraria;
-
-public interface Produto {
-	public String getNome(String nome);
-	public String getDescricao(String descricao);
-	public double getValor(double valor);
-	public void Escreve();
-}
