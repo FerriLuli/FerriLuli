e02e40d2176402678cf77070857e3053695b0b25
FerriLuli <110925223+FerriLuli@users.noreply.github.com>
2022-09-02 21:50:36 -0300

    Add files via upload

---
+++ FIRST SQL PROJECT - CLASS 1.sql
@@ -0,0 +1,70 @@
+CREATE SCHEMA aula3;
+
+USE aula3;
+CREATE TABLE RECEITUARIO (CODIGO VARCHAR(100)
+						, NOME_MEDICO       VARCHAR(100)
+                        , CRO               INT
+                        , ESPECIALIDADE     VARCHAR(100)
+                        , NOME_PACIENTE     VARCHAR(100)
+                        , ENDERECO_PACIENTE VARCHAR(100)
+                        , MEDICAMENTO       VARCHAR(100)
+                        , DOSE              VARCHAR(100)
+                        , FREQUENCIA        VARCHAR(100)
+                        , DATA_EMISSAO      DATE
+						);
+                        
+INSERT INTO RECEITUARIO (DATA_EMISSAO)
+						VALUES(STR_TO_DATE('01/09/2022', "%d/%m/%Y"));
+                        
+INSERT INTO RECEITUARIO (RECEITUARIO_ID
+					    , CODIGO
+						, NOME_MEDICO
+                        , CRO
+                        , ESPECIALIDADE
+                        , NOME_PACIENTE
+                        , ENDERECO_PACIENTE
+                        , MEDICAMENTO
+                        , DOSE
+                        , FREQUENCIA
+                        , DATA_EMISSAO
+                        )
+                        VALUE (NULL
+							  ,	'1'
+							  , 'PAULO'
+                              , '123456789'
+                              , 'CARDIOLOGISTA'
+                              , 'ANA'
+                              , 'RUA 1, 12, MUNICIPIO, ESTADO'
+                              , 'LOSARTANA POTÁSSICA'
+                              , '1 COMPRIMIDO'
+                              , '2 VEZES POR DIA'
+                              , STR_TO_DATE('03/09/2022', "%d/%m/%Y")
+							  );
+                              
+ALTER TABLE RECEITUARIO DROP COLUMN CODIGO;
+
+ALTER TABLE RECEITUARIO ADD COLUMN CODIGO VARCHAR(100);
+
+INSERT INTO RECEITUARIO (CODIGO)
+						VALUES('1');
+                        
+ALTER TABLE RECEITUARIO ADD COLUMN RECEITUARIO_ID INT PRIMARY KEY AUTO_INCREMENT;
+
+ALTER TABLE RECEITUARIO ADD CONSTRAINT UNIQUE (CODIGO
+											 , NOME_MEDICO
+                                             , CRO
+                                             );
+ 
+SELECT DATA_EMISSAO 
+	FROM RECEITUARIO 
+		WHERE DATA_EMISSAO = '2022/09/01';
+        
+SELECT DATA_EMISSAO 
+	FROM RECEITUARIO 
+		ORDER BY DATA_EMISSAO;
+
+SELECT ((2+2)/(2));
+
+SELECT *
+	FROM RECEITUARIO;
+
